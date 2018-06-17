201b999fa89e70249e925ee2f8e0d9457cd6bd27
kde6260 <kde6260@gmail.com>
2018-06-18 01:52:13 +0900

    feat: add post-response and error-handling middlewares

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+
 'use strict';
 
 const compress = require('koa-compress');
 const logger = require('koa-logger');
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const index = require('./controllers/index');
 const app = new Koa();
 
-const index = require('./controllers/index');
 
-// Logger
+app.use(bodyParser());
 app.use(logger());
 
-
+//centralized error
+app.use(async (ctx, next) => {
+    try {
+        await next();
+    } catch (err) {
+        ctx.status = err.status || 500;
+        ctx.body = err.message;
+        ctx.app.emit('error', err, ctx);
+    }
+});
 app.use(index.routes());
 app.use(index.allowedMethods());
 
@@ -22,6 +33,6 @@ app.use((ctx, next) => {
 });
 
 if (!module.parent) {
-  app.listen(3000);
-  console.log('listening on port 3000');
+  app.listen(6260);
+  console.log('listening on port 6260');
 }
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,33 @@
-const Koa = require('koa');
 const Router = require('koa-router');
 const router = new Router();
-
-router.get('/', (ctx, next) => {
-    ctx.body = 'Hi, koa.';
+const multer = require('koa-multer');
+const upload = multer({
+    dest: '../mpy-projects/'
 });
 
 
+//upload 에러는 centralized error에서 처리됨
+router.post('/project', upload.single('file'), async (ctx, next)=>{
+   try{
+       //파일업로드 & 소나스캔 처리하는 미들웨어
+       next();
+   }catch(err){
+       ctx.throw(500, new Error('sonar scan error'));
+   }
+});
+
+router.use(async(ctx, next)=>{
+    //postgreSQL에서 스캔결과 가져오고 응답하는 미들웨어
+    try{
+
+    }catch(err){
+        ctx.throw(500, new Error('select result error'));
+    }
+});
+
+
+
 module.exports = router;
+
+
+
